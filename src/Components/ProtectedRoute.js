d6fa7819d18b37344941078515ae9c99eec0de42
sakthi <sakthipalanisamy7@gmail.com>
2024-08-30 21:55:27 +0530

    User not logged in while reloading page issue resolved

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -1,13 +1,35 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { auth } from '../config/firebase';
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  // const { user } = useContext(AuthContext);
+  let user;
+  const fetchUser = async () => {
+    
+    const fetchUserDetails = () =>
+      {
+        user=auth.currentUser;
+        console.log(user);
+      }
+    
+      await fetchUserDetails();
+      
+  }
+
+  fetchUser()
+
   const navigate = useNavigate();
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
 
   useEffect(() => {
+    if (user === null) {
+      // Handle loading state, if necessary
+      return ;
+    }
+    
     if (!user) {
       toast.error('You must be logged in to access this page.');
       const timer = setTimeout(() => {
@@ -15,8 +37,14 @@ const ProtectedRoute = ({ children }) => {
       }, 5000);
 
       return () => clearTimeout(timer);
+    } else {
+      setIsCheckingAuth(false);
     }
   }, [user, navigate]);
+
+  if (isCheckingAuth) {
+    return <div>Loading...</div>; 
+  }
 
   if (!user) {
     return (
